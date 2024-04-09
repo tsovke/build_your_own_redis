25a2e2294f4ca1e2205c16938172e99789b86869
tsovke <63042183+tsovke@users.noreply.github.com>
2024-04-09 17:12:09 +0800

    ok

--- 06/06_server.cpp
+++ 06/06_server.cpp
@@ -1,9 +1,12 @@
 #include <arpa/inet.h>
+#include <asm-generic/socket.h>
 #include <assert.h>
+#include <cerrno>
 #include <cstddef>
 #include <cstdlib>
 #include <errno.h>
 #include <fcntl.h>
+#include <math.h>
 #include <netinet/in.h>
 #include <netinet/ip.h>
 #include <poll.h>
@@ -11,6 +14,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/poll.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
@@ -152,4 +156,165 @@ static bool try_fill_buffer(Conn *conn) {
     rv = read(conn->fd, &conn->rbuf[conn->rbuf_size], cap);
 
   } while (rv < 0 && errno == EINTR);
+
+  if (rv < 0 && errno == EAGAIN) {
+    // got EAGAIN, stop.
+    return false;
+  }
+  if (rv < 0) {
+    msg("read() error");
+    conn->state = STATE_END;
+    return false;
+  }
+  if (rv == 0) {
+    if (conn->rbuf_size > 0) {
+      msg("unexpected EOF");
+
+    } else {
+      msg("EOF");
+    }
+    conn->state = STATE_END;
+    return false;
+  }
+
+  conn->rbuf_size += (size_t)rv;
+  assert(conn->rbuf_size <= sizeof(conn->rbuf));
+
+  // Try to process requests one by one.
+  // Why is there a loop? Please read the explanation of "pipelinling".
+  while (try_one_request(conn)) {
+  }
+  return (conn->state == STATE_REQ);
+}
+
+static void state_req(Conn *conn) {
+  while (try_fill_buffer(conn)) {
+  }
+}
+
+static bool try_flush_buffer(Conn *conn) {
+  ssize_t rv = 0;
+  do {
+    size_t remain = conn->wbuf_size - conn->wbuf_sent;
+    rv = write(conn->fd, &conn->wbuf[conn->wbuf_sent], remain);
+
+  } while (rv < 0 && errno == EINTR);
+
+  if (rv < 0 && errno == EAGAIN) {
+    // got EAGAIN, stop.
+    return false;
+  }
+  if (rv < 0) {
+    msg("write() error");
+    conn->state = STATE_END;
+    return false;
+  }
+  conn->wbuf_sent += (size_t)rv;
+  assert(conn->wbuf_sent <= conn->wbuf_size);
+  if (conn->wbuf_sent == conn->wbuf_size) {
+    // response was fully sent, change state back
+    conn->state = STATE_REQ;
+    conn->wbuf_sent = 0;
+    conn->wbuf_size = 0;
+    return false;
+  }
+  // still got some data in wbuf, could try to write again
+  return true;
+}
+
+static void state_res(Conn *conn) {
+  while (try_flush_buffer(conn)) {
+  }
+}
+
+static void connection_io(Conn *conn) {
+  if (conn->state == STATE_REQ) {
+    state_req(conn);
+
+  } else if (conn->state == STATE_RES) {
+    state_res(conn);
+
+  } else {
+    assert(0); // not expected
+  }
+}
+
+int main() {
+  int fd = socket(AF_INET, SOCK_STREAM, 0);
+  if (fd < 0) {
+    die("socket()");
+  }
+
+  int val = 1;
+  setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));
+
+  // bind
+  struct sockaddr_in addr = {};
+  addr.sin_family = AF_INET;
+  addr.sin_port = ntohs(1234);
+  addr.sin_addr.s_addr = ntohl(0); // wildcard address 0.0.0.0
+  int rv = bind(fd, (const sockaddr *)&addr, sizeof(addr));
+  if (rv) {
+    die("bind()");
+  }
+
+  // listen
+  rv = listen(fd, SOMAXCONN);
+  if (rv) {
+    die("listen()");
+  }
+
+  // a map of all client connections, keyed by fd
+  std::vector<Conn *> fd2conn;
+
+  // set the listen fd to nonblocking mode
+  fd_set_nb(fd);
+
+  // the event loop
+  std::vector<struct pollfd> poll_args;
+  while (true) {
+    // prepare the arguments of the poll()
+    poll_args.clear();
+    // for convenience, the listening fd is put in the first posit
+    struct pollfd pfd = {fd, POLLIN, 0};
+    poll_args.push_back(pfd);
+    // connection fds
+    for (Conn *conn : fd2conn) {
+      if (!conn) {
+        continue;
+      }
+      struct pollfd pfd = {};
+      pfd.fd = conn->fd;
+      pfd.events = (conn->state == STATE_REQ) ? POLLIN : POLLOUT;
+      pfd.events |= POLLERR;
+      poll_args.push_back(pfd);
+    }
+
+    // poll for active fds
+    // the timeout argument doesn't matter here
+    int rv = poll(poll_args.data(), (nfds_t)poll_args.size(), 1000);
+    if (rv < 0) {
+      die("poll");
+    }
+    // process active connections
+    for (size_t i = 1; i < poll_args.size(); ++i) {
+      if (poll_args[i].revents) {
+        Conn *conn = fd2conn[poll_args[i].fd];
+        connection_io(conn);
+        if (conn->state == STATE_END) {
+          // client closed normally, or something bad happened.
+          //  destroy this connection
+          fd2conn[conn->fd] = NULL;
+          (void)close(conn->fd);
+          free(conn);
+        }
+      }
+    }
+    // tree to accept a new connection if the listening fd is active
+    if (poll_args[0].revents) {
+      (void)accept_new_conn(fd2conn, fd);
+    }
+  }
+
+  return 0;
 }
