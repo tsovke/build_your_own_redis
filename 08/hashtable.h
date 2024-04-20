83a76f094188f67128783e9b0919a608290748df
tsovke <63042183+tsovke@users.noreply.github.com>
2024-04-20 16:09:38 +0800

    Update hashtable.h

--- 08/hashtable.h
+++ 08/hashtable.h
@@ -1,6 +1,20 @@
 #pragma once
 
+#include <cstdint>
 #include <stddef.h>
 #include <stdint.h>
 
+
+// hashtable, node, should be embeded into the payload
+struct HNode {
+  HNode *next=NULL;
+  uint64_t hcode = 0;
+};
+
+// a simple fixed-sized hashtable
+struct Htab {
+  HNode **tab=NULL;
+  size_t mask =0;
+  size_t size = 0;
+};
 
