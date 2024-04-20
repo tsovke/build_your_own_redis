579c7f1ba40af0032caf110dc4c5b02b57cf9644
tsovke <63042183+tsovke@users.noreply.github.com>
2024-04-20 18:03:04 +0800

    Update hashtable.cpp

--- 08/hashtable.cpp
+++ 08/hashtable.cpp
@@ -1,14 +1,22 @@
+#include "hashtable.h"
 #include <assert.h>
 #include <cstddef>
 #include <cstdlib>
 #include <stdlib.h>
-#include "hashtable.h"
-
 
 // n must be a power of 2
-static void h_init(HTab *htab, size_t n){
-  assert(n>0 && ((n-1)&n)==0);
-  htab->tab=(HNode **)calloc(sizeof(HNode *), n);
-  htab->mask =n-1;
-  htab->size=0;
+static void h_init(HTab *htab, size_t n) {
+  assert(n > 0 && ((n - 1) & n) == 0);
+  htab->tab = (HNode **)calloc(sizeof(HNode *), n);
+  htab->mask = n - 1;
+  htab->size = 0;
+}
+
+// hashtable insertion
+static void hm_insert(HTab *htab, HNode *node) {
+  size_t pos = node->hcode & htab->mask;
+  HNode *next = htab->tab[pos];
+  node->next = next;
+  htab->tab[pos] = node;
+  htab->size++;
 }
