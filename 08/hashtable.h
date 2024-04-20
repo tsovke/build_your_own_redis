a527eab2434c91e691028643ff3dff85b33a6f93
tsovke <63042183+tsovke@users.noreply.github.com>
2024-04-20 16:25:36 +0800

    Update hashtable.h

--- 08/hashtable.h
+++ 08/hashtable.h
@@ -4,17 +4,23 @@
 #include <stddef.h>
 #include <stdint.h>
 
-
 // hashtable, node, should be embeded into the payload
 struct HNode {
-  HNode *next=NULL;
+  HNode *next = NULL;
   uint64_t hcode = 0;
 };
 
 // a simple fixed-sized hashtable
-struct Htab {
-  HNode **tab=NULL;
-  size_t mask =0;
+struct HTab {
+  HNode **tab = NULL;
+  size_t mask = 0;
   size_t size = 0;
 };
 
+// the real hashtable interface.
+// it uses 2 hashtables for progressive resizing.
+struct HMap {
+  HTab ht1; // newer
+  HTab ht2; // older
+  size_t resizing_pos = 0;
+};
