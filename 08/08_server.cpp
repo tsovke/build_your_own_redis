e8bf2cd434bbaf8a4ca747451f56700e07b86f7b
tsovke <63042183+tsovke@users.noreply.github.com>
2024-04-21 00:08:14 +0800

    Update 08_server.cpp

--- 08/08_server.cpp
+++ 08/08_server.cpp
@@ -158,3 +158,17 @@ struct Entry {
   std::string key;
   std::string val;
 };
+
+static bool entry_eq(HNode *lhs, HNode *rhs) {
+  struct Entry *le = container_of(lhs, struct Entry, node);
+  struct Entry *re = container_of(rhs, struct Entry, node);
+  return le->key == re->key;
+}
+
+static uint64_t str_hash(const uint8_t *data, size_t len) {
+  uint32_t h = 0x811C9DC5;
+  for (size_t i = 0; i < len; ++i) {
+    h = (h + data[i]) * 0x01000193;
+  }
+  return h;
+}
