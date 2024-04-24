1e50c7f1a0aab13cac85192df522e05e1bc32bae
tsovke <63042183+tsovke@users.noreply.github.com>
2024-04-24 21:05:51 +0800

    Update hashtable.cpp

---
+++ 09/hashtable.cpp
@@ -0,0 +1,14 @@
+#include "hashtable.h"
+#include <assert.h>
+#include <cstddef>
+#include <cstdlib>
+#include <endian.h>
+#include <stdlib.h>
+
+// n must be a power of 2
+static void h_init(HTab *htab, size_t n) {
+  assert(n > 0 && ((n - 1) & n) == 0);
+  htab->tab = (HNode **)calloc(sizeof(HNode *), n);
+  htab->mask = n - 1;
+  htab->size = 0;
+}
