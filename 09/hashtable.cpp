bc63627d62937adfac8360f8d6fc9c75fe0f49ac
tsovke <63042183+tsovke@users.noreply.github.com>
2024-04-24 22:12:18 +0800

    Update hashtable.cpp

--- 09/hashtable.cpp
+++ 09/hashtable.cpp
@@ -1,5 +1,6 @@
 #include "hashtable.h"
 #include <assert.h>
+#include <charconv>
 #include <cstddef>
 #include <cstdlib>
 #include <endian.h>
@@ -44,9 +45,32 @@ static HNode **h_lookup(HTab *htab, HNode *key, bool (*eq)(HNode *, HNode *)) {
 }
 
 // remove a node from the chain
-static HNode *h_detach(HTab *htab,HNode **from){
-  HNode *node=*from;
+static HNode *h_detach(HTab *htab, HNode **from) {
+  HNode *node = *from;
   *from = node->next;
   htab->size--;
   return node;
+}
+
+const size_t k_resizing_work = 128; // constant work
+
+static void hm_help_resizing(HMap *hmap) {
+  size_t nwork = 0;
+  while (nwork < k_resizing_work && hmap->ht2.size > 0) {
+    // scan for nodes from ht2 and move them to ht1
+    HNode **from = &hmap->ht2.tab[hmap->resizing_pos];
+    if (!*from) {
+      hmap->resizing_pos++;
+      continue;
+    }
+
+    h_insert(&hmap->ht1, h_detach(&hmap->ht2, from));
+    nwork++;
+  }
+
+  if (hmap->ht2.size == 0 && hmap->ht2.tab) {
+    // done
+    free(hmap->ht2.tab);
+    hmap->ht2 = HTab{};
+  }
 }
