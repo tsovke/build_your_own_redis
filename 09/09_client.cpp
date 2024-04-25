a3e2d54bf8e22689835c465891c9dc2fdb38fc1d
tsovke <63042183+tsovke@users.noreply.github.com>
2024-04-25 18:00:57 +0800

    Update 09_client.cpp

--- 09/09_client.cpp
+++ 09/09_client.cpp
@@ -55,5 +55,24 @@ static int32_t write_all(int fd,const char *buf,size_t n){
 const size_t k_max_msg=4096;
 
 static int32_t send_req(int fd,const std::vector<std::string> &cmd){
-  
+  uint32_t len=4;
+  for (const std::string &s:cmd ) {
+    len+=4+s.size();
+  }
+  if (len>k_max_msg) {
+    return -1;
+  }
+
+  char wbuf[4+k_max_msg];
+  memcpy(&wbuf[0],&len ,4 );//assume little endian
+  uint32_t n=cmd.size();
+  memcpy(&wbuf[4],&n ,4 );
+  size_t cur=8;
+  for ( const std::string &s:cmd ) {
+    uint32_t p = (uint32_t)s.size();
+    memcpy(&wbuf[cur],&p ,4 );
+    memcpy(&wbuf[cur+4], s.data(),s.size() );
+    cur+=4+s.size();
+  }
+  return write_all(fd,wbuf ,4+len );
 }
