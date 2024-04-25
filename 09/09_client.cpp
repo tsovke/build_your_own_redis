e10a25b29699ff72c50a3abdd256c64769cd2579
tsovke <63042183+tsovke@users.noreply.github.com>
2024-04-25 17:40:22 +0800

    Create 09_client.cpp

---
+++ 09/09_client.cpp
@@ -0,0 +1,59 @@
+#include <arpa/inet.h>
+#include <assert.h>
+#include <cstddef>
+#include <cstdint>
+#include <cstdlib>
+#include <ctime>
+#include <errno.h>
+#include <netinet/ip.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <string>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <vector>
+
+static void msg(const char *msg) { fprintf(stderr, "%s\n", msg); }
+
+static void die(const char *msg) {
+  int err = errno;
+  fprintf(stderr, "[%d] %s\n", err, msg);
+  abort();
+}
+
+static int32_t read_full(int fd, char *buf, size_t n) {
+  while (n > 0) {
+    ssize_t rv = read(fd, buf, n);
+    if (rv <= 0) {
+      return -1; // error, or unexpected EOF
+    }
+    assert((size_t)rv <= n);
+    n -= (size_t)rv;
+    buf += rv;
+  }
+  return 0;
+}
+
+static int32_t write_all(int fd,const char *buf,size_t n){
+  while (n > 0) {
+    ssize_t rv = write(fd, buf, n);
+    if (rv <= 0) {
+      return -1; // error
+    }
+    assert((size_t)rv <= n);
+    n -= (size_t)rv;
+    buf += rv;
+  }
+  return 0;
+
+  
+}
+
+const size_t k_max_msg=4096;
+
+static int32_t send_req(int fd,const std::vector<std::string> &cmd){
+  
+}
