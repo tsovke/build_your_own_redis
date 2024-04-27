512ffa633ae81efb821a48118620b8cea4f89e0d
tsovke <63042183+tsovke@users.noreply.github.com>
2024-04-27 18:12:16 +0800

    Create avl.cpp

---
+++ 10/avl.cpp
@@ -0,0 +1,66 @@
+#include <cstdint>
+#include <ctime>
+#include <regex>
+#include <stddef.h>
+#include <stdint.h>
+#include <system_error>
+
+struct AVLNode {
+  uint32_t depth = 0;
+  uint32_t cnt = 0;
+  AVLNode *left = NULL;
+  AVLNode *right = NULL;
+  AVLNode *parent = NULL;
+};
+
+static void avl_init(AVLNode *node) {
+  node->depth = 1;
+  node->cnt = 1;
+  node->left = node->right = node->parent = NULL;
+}
+
+static uint32_t avl_depth(AVLNode *node) { return node ? node->depth : 0; }
+
+static uint32_t avl_cnt(AVLNode *node) { return node ? node->cnt : 0; }
+
+static uint32_t max(uint32_t lhs, uint32_t rhs){
+  return lhs<rhs?rhs:lhs;
+}
+
+// maintaining the depth and cnt field 
+static void avl_update(AVLNode *node){
+  node->depth=1+max(avl_depth(node->left),avl_depth(node->right) );
+  node->cnt=1+avl_cnt(node->left)+avl_cnt(node->right);
+}
+
+static AVLNode *rot_left(AVLNode *node){
+  AVLNode *new_node=node->right;
+  if (new_node->left) {
+    new_node->left->parent=node;
+  }
+
+  node->right=new_node->left;
+  new_node->left=node;
+  new_node->parent=node->parent;
+  node->parent=new_node;
+  avl_update(node);
+  avl_update(new_node);
+  return new_node;
+}
+
+static AVLNode *rot_right(AVLNode *node){
+  AVLNode *new_node=node->left;
+  if (new_node->right) {
+    new_node->right->parent=node;
+  }
+
+  node->left=new_node->right;
+  new_node->right=node;
+  new_node->parent=node->parent;
+  node->parent=new_node;
+  avl_update(node);
+  avl_update(new_node);
+  return new_node;
+} 
+
+// the left
