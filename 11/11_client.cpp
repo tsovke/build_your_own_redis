aa0ddb04b09abafd16efa109077c1ed5212a22ed
tsovke <63042183+tsovke@users.noreply.github.com>
2024-05-02 16:24:24 +0800

    Update 11_client.cpp

--- 11/11_client.cpp
+++ 11/11_client.cpp
@@ -1,14 +1,25 @@
+#include <arpa/inet.h>
 #include <assert.h>
+#include <bits/types/error_t.h>
+#include <cstdlib>
+#include <errno.h>
+#include <netinet/ip.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
-#include <errno.h>
-#include <unistd.h>
-#include <arpa/inet.h>
-#include <sys/socket.h>
-#include <netinet/ip.h>
 #include <string>
+#include <sys/socket.h>
+#include <unistd.h>
 #include <vector>
 // proj
 #include "common.h"
+
+static void msg(const char *msg) { fprintf(stderr, "%s\n", msg); }
+
+static void die(const char *msg) {
+  int err = errno;
+
+  fprintf(stderr, "[%d] %s\n", err, msg);
+  abort();
+}
