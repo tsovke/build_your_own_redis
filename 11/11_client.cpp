4897a22f4d202a4544aae9de780d7d8cd9e83ba3
tsovke <63042183+tsovke@users.noreply.github.com>
2024-05-02 16:32:42 +0800

    Update 11_client.cpp

--- 11/11_client.cpp
+++ 11/11_client.cpp
@@ -1,6 +1,8 @@
 #include <arpa/inet.h>
 #include <assert.h>
 #include <bits/types/error_t.h>
+#include <cstddef>
+#include <cstdint>
 #include <cstdlib>
 #include <errno.h>
 #include <netinet/ip.h>
@@ -10,6 +12,7 @@
 #include <string.h>
 #include <string>
 #include <sys/socket.h>
+#include <sys/types.h>
 #include <unistd.h>
 #include <vector>
 // proj
@@ -19,7 +22,32 @@ static void msg(const char *msg) { fprintf(stderr, "%s\n", msg); }
 
 static void die(const char *msg) {
   int err = errno;
-
   fprintf(stderr, "[%d] %s\n", err, msg);
   abort();
+}
+
+static int32_t read_full(int fd, char *buf, size_t n) {
+  while (n > 0) {
+    ssize_t rv = read(fd, buf, n);
+    if (rv <= 0) {
+      return -1; // error, or unexpected EOF
+    }
+    assert((size_t)rv <= n);
+    n -= (size_t)rv;
+    buf += rv;
+  }
+  return 0;
+}
+
+static int32_t write_full(int fd, char *buf, size_t n) {
+  while (n > 0) {
+    ssize_t rv = write(fd, buf, n);
+    if (rv <= 0) {
+      return -1; // error
+    }
+    assert((size_t)rv <= n);
+    n -= (size_t)rv;
+    buf += rv;
+  }
+  return 0;
 }
