8ccc560351e2d1251a1f98d2bd011707bfdafebf
tsovke <63042183+tsovke@users.noreply.github.com>
2024-05-03 17:05:15 +0800

    Update 11_server.cpp

--- 11/11_server.cpp
+++ 11/11_server.cpp
@@ -1,8 +1,11 @@
 #include <arpa/inet.h>
 #include <assert.h>
+#include <cstddef>
+#include <cstdlib>
 #include <errno.h>
 #include <fcntl.h>
 #include <math.h>
+#include <netinet/in.h>
 #include <netinet/ip.h>
 #include <poll.h>
 #include <stdint.h>
@@ -18,50 +21,81 @@
 #include "hashtable.h"
 #include "zset.h"
 
-static void msg(const char *msg) {
-    fprintf(stderr, "%s\n", msg);
-}
+static void msg(const char *msg) { fprintf(stderr, "%s\n", msg); }
 
 static void die(const char *msg) {
-    int err = errno;
-    fprintf(stderr, "[%d] %s\n", err, msg);
-    abort();
+  int err = errno;
+  fprintf(stderr, "[%d] %s\n", err, msg);
+  abort();
 }
 
 static void fd_set_nb(int fd) {
-    errno = 0;
-    int flags = fcntl(fd, F_GETFL, 0);
-    if (errno) {
-        die("fcntl error");
-        return;
-    }
+  errno = 0;
+  int flags = fcntl(fd, F_GETFL, 0);
+  if (errno) {
+    die("fcntl error");
+    return;
+  }
 
-    flags |= O_NONBLOCK;
+  flags |= O_NONBLOCK;
 
-    errno = 0;
-    (void)fcntl(fd, F_SETFL, flags);
-    if (errno) {
-        die("fcntl error");
-    }
+  errno = 0;
+  (void)fcntl(fd, F_SETFL, flags);
+  if (errno) {
+    die("fcntl error");
+  }
 }
 
 const size_t k_max_msg = 4096;
 
 enum {
-    STATE_REQ = 0,
-    STATE_RES = 1,
-    STATE_END = 2,  // mark the connection for deletion
+  STATE_REQ = 0,
+  STATE_RES = 1,
+  STATE_END = 2, // mark the connection for deletion
 };
 
 struct Conn {
-    int fd = -1;
-    uint32_t state = 0;     // either STATE_REQ or STATE_RES
-    // buffer for reading
-    size_t rbuf_size = 0;
-    uint8_t rbuf[4 + k_max_msg];
-    // buffer for writing
-    size_t wbuf_size = 0;
-    size_t wbuf_sent = 0;
-    uint8_t wbuf[4 + k_max_msg];
+  int fd = -1;
+  uint32_t state = 0; // either STATE_REQ or STATE_RES
+  // buffer for reading
+  size_t rbuf_size = 0;
+  uint8_t rbuf[4 + k_max_msg];
+  // buffer for writing
+  size_t wbuf_size = 0;
+  size_t wbuf_sent = 0;
+  uint8_t wbuf[4 + k_max_msg];
 };
 
+static void conn_put(std::vector<Conn *> &fd2conn, struct Conn *conn) {
+  if (fd2conn.size() <= (size_t)conn->fd) {
+    fd2conn.resize(conn->fd + 1);
+  }
+  fd2conn[conn->fd] = conn;
+}
+
+static int32_t accept_new_conn(std::vector<Conn *> &fd2conn, int fd) {
+  // accept
+  struct sockaddr_in client_addr = {};
+  socklen_t socklen = sizeof(client_addr);
+  int connfd = accept(fd, (struct sockaddr *)&client_addr, &socklen);
+  if (connfd < 0) {
+    msg("accept() error");
+    return -1; // error
+  }
+
+  // set the new connection fd to nonblocking mode
+  fd_set_nb(connfd);
+  // creating the struct Conn
+  struct Conn *conn = (struct Conn *)malloc(sizeof(struct Conn));
+  if (!conn) {
+    close(connfd);
+    return -1;
+  }
+  conn->fd = connfd;
+  conn->state = STATE_REQ;
+  conn->rbuf_size = 0;
+  conn->wbuf_size = 0;
+  conn->wbuf_sent = 0;
+  conn_put(fd2conn, conn);
+  return 0;
+}
