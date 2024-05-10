236746d2f0da4f863ede1473993b5343c3c87cb9
tsovke <63042183+tsovke@users.noreply.github.com>
2024-05-10 09:38:18 +0800

    Update list.h

--- 12/list.h
+++ 12/list.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cstdlib>
 #include <stddef.h>
 
 struct DList {
@@ -12,4 +13,17 @@ inline void dlist_init(DList *node) { node->prev = node->next = node; }
 
 inline bool dlist_empty(DList *node) { return node->next == node; }
 
+inline void dlist_detach(DList *node) {
+  DList *prev = node->prev;
+  DList *next = node->next;
+  prev->next = next;
+  next->prev = prev;
+}
 
+inline void dlist_insert_before(DList *target, DList *rookie) {
+  DList *prev = target->prev;
+  prev->next = rookie;
+  rookie->prev = prev;
+  rookie->next = target;
+  target->prev = rookie;
+}
