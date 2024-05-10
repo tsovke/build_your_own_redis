951657f83262e81bcd9c138da567772c6bac2bb3
tsovke <63042183+tsovke@users.noreply.github.com>
2024-05-10 10:10:37 +0800

    Update zset.cpp

--- 12/zset.cpp
+++ 12/zset.cpp
@@ -7,6 +7,7 @@
 // proj
 #include "avl.h"
 #include "common.h"
+#include "hashtable.h"
 #include "zset.h"
 
 static ZNode *znode_new(const char *name, size_t len, double score) {
@@ -37,3 +38,45 @@ static bool zless(AVLNode *lhs, double score, const char *name, size_t len) {
   return zl->len < len;
 }
 
+static bool zless(AVLNode *lhs, AVLNode *rhs) {
+  ZNode *zr = container_of(rhs, ZNode, tree);
+  return zless(lhs, zr->score, zr->name, zr->len);
+}
+
+// insert into the AVL tree
+static void tree_add(ZSet *zset, ZNode *node) {
+  AVLNode *cur = NULL;          // current node
+  AVLNode **from = &zset->tree; // the incoming pointer to the next node
+  while (*from) {               // tree search
+    cur = *from;
+    from = zless(*node->tree, cur) ? &cur->left : &cur->right;
+  }
+  *from = &node->tree; // attach the new node
+  node->tree.parent = cur;
+  zset->tree = avl_fix(&node->tree);
+}
+
+// update the score of an existing node(AVL tree reinsertion)
+static void zset_update(ZSet *zset, ZNode *node, double score) {
+  if (node->score == score) {
+    return;
+  }
+  zset->tree = avl_del(&node->tree);
+  node->score = score;
+  avl_init(&node->tree);
+  tree_add(zset, node);
+}
+
+// add a new (score,name) tuple, or update the score of the existing tuple
+bool zset_add(ZSet *zset, const char *name, size_t len, double score) {
+  ZNode *node = zset_lookup(zset, name, len);
+  if (node) {
+    zset_update(zset, node, score);
+    return false;
+  } else {
+    node = znode_new(name, len, score);
+    hm_insert(&zset->hmap, &node->hmap);
+    tree_add(zset, node);
+    return true;
+  }
+}
