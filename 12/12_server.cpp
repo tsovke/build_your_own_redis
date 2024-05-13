6ca23ef87aed1ba7037164a388453f25838b2c40
tsovke <63042183+tsovke@users.noreply.github.com>
2024-05-13 10:06:07 +0800

    Update 12_server.cpp

--- 12/12_server.cpp
+++ 12/12_server.cpp
@@ -1,6 +1,8 @@
 #include <arpa/inet.h>
 #include <assert.h>
+#include <cstdint>
 #include <cstdlib>
+#include <ctime>
 #include <errno.h>
 #include <fcntl.h>
 #include <math.h>
@@ -29,3 +31,29 @@ static void die(const char *msg) {
   abort();
 }
 
+static uint64_t get_monotonic_usec() {
+  timespec tv = {0, 0};
+  clock_gettime(CLOCK_MONOTONIC, &tv);
+  return uint64_t(tv.tv_sec) * 1000000 + tv.tv_nsec / 1000;
+}
+
+static void fd_set_nb(int fd) {
+  errno = 0;
+  int flags = fcntl(fd, F_GETFL, 0);
+  if (errno) {
+    die("fcntl error");
+    return;
+  }
+
+  flags |= O_NONBLOCK;
+
+  errno = 0;
+  (void)fcntl(fd, F_SETFL, flags);
+  if (errno) {
+    die("fcntl error");
+  }
+}
+
+struct Conn;
+
+//global variables
