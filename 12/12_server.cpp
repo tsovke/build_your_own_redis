0eade044ed6b5424921fc0338693b1dc939cfadc
tsovke <63042183+tsovke@users.noreply.github.com>
2024-05-14 08:50:07 +0800

    Update 12_server.cpp

--- 12/12_server.cpp
+++ 12/12_server.cpp
@@ -67,3 +67,27 @@ static struct {
 } g_data;
 
 const size_t k_max_msg = 4096;
+
+enum {
+  STATE_REQ = 0,
+  STATE_RES = 1,
+  STATE_END = 2, // mark the connection for deletion
+};
+
+struct Conn {
+
+  int fd = -1;
+  uint32_t state = 0; // either STATE_REQ or STATE_RES
+  // buffer for reading
+  size_t rbuf_size = 0;
+  uint8_t rbuf[4 + k_max_msg];
+  // buffer writing
+
+  size_t wbuf_size = 0;
+  size_t wbuf_sent = 0;
+
+  uint8_t wbuf[4 + k_max_msg];
+  uint64_t idle_start = 0;
+  // timer
+  DList idle_list;
+};
