b5ebdb395ae38afeb9a3790989508fd21467daf9
tsovke <63042183+tsovke@users.noreply.github.com>
2024-05-18 23:03:26 +0800

    Update 12_server.cpp

--- 12/12_server.cpp
+++ 12/12_server.cpp
@@ -471,3 +471,26 @@ static void do_zquery(std::vector<std::string> &cmd, std::string &out) {
 static bool cmd_is(const std::string &word,const char *cmd){
   return 0==strcasecmp(word.c_str(),cmd );
 }
+
+static void do_request(std::vector<std::string> &cmd, std::string &out) {
+    if (cmd.size() == 1 && cmd_is(cmd[0], "keys")) {
+        do_keys(cmd, out);
+    } else if (cmd.size() == 2 && cmd_is(cmd[0], "get")) {
+        do_get(cmd, out);
+    } else if (cmd.size() == 3 && cmd_is(cmd[0], "set")) {
+        do_set(cmd, out);
+    } else if (cmd.size() == 2 && cmd_is(cmd[0], "del")) {
+        do_del(cmd, out);
+    } else if (cmd.size() == 4 && cmd_is(cmd[0], "zadd")) {
+        do_zadd(cmd, out);
+    } else if (cmd.size() == 3 && cmd_is(cmd[0], "zrem")) {
+        do_zrem(cmd, out);
+    } else if (cmd.size() == 3 && cmd_is(cmd[0], "zscore")) {
+        do_zscore(cmd, out);
+    } else if (cmd.size() == 6 && cmd_is(cmd[0], "zquery")) {
+        do_zquery(cmd, out);
+    } else {
+        // cmd is not recognized
+        out_err(out, ERR_UNKNOWN, "Unknown cmd");
+    }
+}
