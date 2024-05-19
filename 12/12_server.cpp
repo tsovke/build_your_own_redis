de0e818ffae8dcde864b94305d87d4deeaae5fa0
tsovke <63042183+tsovke@users.noreply.github.com>
2024-05-19 09:31:26 +0800

    Update 12_server.cpp

--- 12/12_server.cpp
+++ 12/12_server.cpp
@@ -468,67 +468,87 @@ static void do_zquery(std::vector<std::string> &cmd, std::string &out) {
   end_arr(out, arr, n);
 }
 
-static bool cmd_is(const std::string &word,const char *cmd){
-  return 0==strcasecmp(word.c_str(),cmd );
+static bool cmd_is(const std::string &word, const char *cmd) {
+  return 0 == strcasecmp(word.c_str(), cmd);
 }
 
 static void do_request(std::vector<std::string> &cmd, std::string &out) {
-    if (cmd.size() == 1 && cmd_is(cmd[0], "keys")) {
-        do_keys(cmd, out);
-    } else if (cmd.size() == 2 && cmd_is(cmd[0], "get")) {
-        do_get(cmd, out);
-    } else if (cmd.size() == 3 && cmd_is(cmd[0], "set")) {
-        do_set(cmd, out);
-    } else if (cmd.size() == 2 && cmd_is(cmd[0], "del")) {
-        do_del(cmd, out);
-    } else if (cmd.size() == 4 && cmd_is(cmd[0], "zadd")) {
-        do_zadd(cmd, out);
-    } else if (cmd.size() == 3 && cmd_is(cmd[0], "zrem")) {
-        do_zrem(cmd, out);
-    } else if (cmd.size() == 3 && cmd_is(cmd[0], "zscore")) {
-        do_zscore(cmd, out);
-    } else if (cmd.size() == 6 && cmd_is(cmd[0], "zquery")) {
-        do_zquery(cmd, out);
-    } else {
-        // cmd is not recognized
-        out_err(out, ERR_UNKNOWN, "Unknown cmd");
-    }
+  if (cmd.size() == 1 && cmd_is(cmd[0], "keys")) {
+    do_keys(cmd, out);
+  } else if (cmd.size() == 2 && cmd_is(cmd[0], "get")) {
+    do_get(cmd, out);
+  } else if (cmd.size() == 3 && cmd_is(cmd[0], "set")) {
+    do_set(cmd, out);
+  } else if (cmd.size() == 2 && cmd_is(cmd[0], "del")) {
+    do_del(cmd, out);
+  } else if (cmd.size() == 4 && cmd_is(cmd[0], "zadd")) {
+    do_zadd(cmd, out);
+  } else if (cmd.size() == 3 && cmd_is(cmd[0], "zrem")) {
+    do_zrem(cmd, out);
+  } else if (cmd.size() == 3 && cmd_is(cmd[0], "zscore")) {
+    do_zscore(cmd, out);
+  } else if (cmd.size() == 6 && cmd_is(cmd[0], "zquery")) {
+    do_zquery(cmd, out);
+  } else {
+    // cmd is not recognized
+    out_err(out, ERR_UNKNOWN, "Unknown cmd");
+  }
 }
 
-static bool try_one_request(Conn *conn){
-  //try to parse a request from the buffer
-  if (conn->rbuf_size<4) {
-    //not enough data in the buffer. Will retry in the next iteration
+static bool try_one_request(Conn *conn) {
+  // try to parse a request from the buffer
+  if (conn->rbuf_size < 4) {
+    // not enough data in the buffer. Will retry in the next iteration
     return false;
   }
-  uint32_t len=0;
-  memcpy(&len,&conn->rbuf[0] ,4 );
-  if (len>k_max_msg) {
+  uint32_t len = 0;
+  memcpy(&len, &conn->rbuf[0], 4);
+  if (len > k_max_msg) {
     msg("too long");
-    conn->state=STATE_END;
+    conn->state = STATE_END;
     return false;
   }
-  if(4+len>conn->rbuf_size){
-    //not enough data in the buffer. Will retry in the next iteration
+  if (4 + len > conn->rbuf_size) {
+    // not enough data in the buffer. Will retry in the next iteration
     return false;
   }
 
-  //parse the request
+  // parse the request
   std::vector<std::string> cmd;
-  if (0!=parse_req(&conn->rbuf[4],len ,cmd )) {
+  if (0 != parse_req(&conn->rbuf[4], len, cmd)) {
     msg("bad req");
-    conn->state=STATE_END;return false;
+    conn->state = STATE_END;
+    return false;
   }
 
-  //got one request, generate the response.
+  // got one request, generate the response.
   std::string out;
-  do_request(cmd,out );
+  do_request(cmd, out);
 
-  //pack the response into the buffer
-  if (4+out.size()>k_max_msg) {
+  // pack the response into the buffer
+  if (4 + out.size() > k_max_msg) {
     out.clear();
-    out_err(out,ERR_2BIG ,"response is too big" );
+    out_err(out, ERR_2BIG, "response is too big");
   }
-  uint32_t wlen=(uint32_t)out.size();
-  memcpy(&conn->wbuf[0],&wlen ,4 );
+  uint32_t wlen = (uint32_t)out.size();
+  memcpy(&conn->wbuf[0], &wlen, 4);
+  memcpy(&conn->wbuf[4], out.data(), out.size());
+  conn->wbuf_size = 4 + wlen;
+
+  // remove the request from the buffer.
+  // note: frequent memmove is inefficient.
+  // note: need better handling for production code.
+  size_t remain = conn->rbuf_size - 4 - len;
+  if (remain) {
+    memmove(conn->rbuf, &conn->rbuf[4 + len], remain);
+  }
+  conn->rbuf_size = remain;
+
+  // change state
+  conn->state = STATE_RES;
+  state_res(conn);
+
+  // continue the outer loop if the request was fully processed
+  return (conn->state == STATE_REQ);
 }
+
