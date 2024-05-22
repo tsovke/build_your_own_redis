63a887b66d8e4a08424529ae1ce96e8d67dd08c3
tsovke <63042183+tsovke@users.noreply.github.com>
2024-05-22 10:04:16 +0800

    Update test_heap.cpp

--- 13/test_heap.cpp
+++ 13/test_heap.cpp
@@ -2,8 +2,10 @@
 #include "heap.h"
 #include <assert.h>
 #include <cstddef>
+#include <cstdint>
 #include <map>
 #include <sys/types.h>
+#include <utility>
 #include <vector>
 
 struct Data {
@@ -19,4 +21,14 @@ static void dispose(Container &c) {
   for (auto p : c.map) {
     delete p.second;
   }
+}
+
+static void add(Container &c, uint64_t val) {
+  Data *d = new Data();
+  c.map.insert(std::make_pair(val, d));
+  HeapItem item;
+  item.ref = &d->heap_idx;
+  item.val = val;
+  c.heap.push_back(item);
+  heap_update(c.heap.data(), c.heap.size() - 1, c.heap.size());
 }
