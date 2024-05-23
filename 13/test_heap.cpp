96d907de21afa51ac3c49c8137000388437e2723
tsovke <63042183+tsovke@users.noreply.github.com>
2024-05-23 22:49:33 +0800

    Update test_heap.cpp

--- 13/test_heap.cpp
+++ 13/test_heap.cpp
@@ -32,3 +32,18 @@ static void add(Container &c, uint64_t val) {
   c.heap.push_back(item);
   heap_update(c.heap.data(), c.heap.size() - 1, c.heap.size());
 }
+
+static void del(Container &c, uint64_t val) {
+  auto it = c.map.find(val);
+  assert(it != c.map.end());
+  Data *d = it->second;
+  assert(c.heap.at(d->heap_idx).val == val);
+  assert(c.heap.at(d->heap_idx).ref == &d->heap_idx);
+  c.heap[d->heap_idx] = c.heap.back();
+  c.heap.pop_back();
+  if (d->heap_idx < c.heap.size()) {
+    heap_update(c.heap.data(), d->heap_idx, c.heap.size());
+  }
+  delete d;
+  c.map.erase(it);
+}
