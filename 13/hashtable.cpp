4507703cbfb92716b0bd34a082ef3d6da60e1ffa
tsovke <63042183+tsovke@users.noreply.github.com>
2024-06-07 01:01:29 +0800

    ok

--- 13/hashtable.cpp
+++ 13/hashtable.cpp
@@ -5,7 +5,7 @@
 // n must be a power of 2
 static void h_init(HTab *htab, size_t n) {
   assert(n > 0 && ((n - 1) & n) == 0);
-  htab->tab = (HNode **)calloc(sizeof(HNode *), n);
+  htab->tab = (HNode **)calloc(n, sizeof(HNode *));
   htab->mask = n - 1;
   htab->size = 0;
 }
