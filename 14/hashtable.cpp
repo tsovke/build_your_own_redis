5af4cb4ebae42a45346201e2ad2289851297a6a3
tsovke <63042183+tsovke@users.noreply.github.com>
2024-06-13 00:27:53 +0800

    Update hashtable.cpp

--- 14/hashtable.cpp
+++ 14/hashtable.cpp
@@ -1,5 +1,6 @@
 #include "hashtable.h"
 #include <assert.h>
+#include <cstddef>
 #include <stdlib.h>
 
 // n must be a power of 2
@@ -7,4 +8,20 @@ static void h_init(HTab *htab, size_t n) {
   assert(n > 0 && ((n - 1) & n) == 0);
   htab->mask = n - 1;
   htab->size = 0;
+}
+
+// hashtable insertion
+static void h_insert(HTab *htab,HNode *node ){
+  size_t pos=node->hcode&htab->mask;
+  HNode *next=htab->tab[pos];
+  node->next=next;htab->tab[pos]=node;
+  htab->size++;
+}
+
+// hashtable look up subroutine.
+// Pay attention to the return value. It returns the address of
+// the parent pointer that owns the target node,
+// which can be used to delete the target node.
+static HNode **h_lookup(){
+  
 }
