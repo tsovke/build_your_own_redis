334970ebad4d3554aa515e6fe0f4a4c3217ea47a
tsovke <63042183+tsovke@users.noreply.github.com>
2024-06-14 22:32:33 +0800

    Update heap.cpp

--- 14/heap.cpp
+++ 14/heap.cpp
@@ -3,3 +3,7 @@
 #include <stdint.h>
 
 static size_t heap_parent(size_t i) { return (i + 1) / 2 - 1; }
+
+static size_t heap_left(size_t i) { return i * 2 + 1; }
+
+static size_t heap_right(size_t i) { return i * 2 + 2; }
