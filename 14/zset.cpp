7949dbf94d557fc4de4a726456e464d1656b3808
tsovke <63042183+tsovke@users.noreply.github.com>
2024-06-15 00:19:24 +0800

    Update zset.cpp

--- 14/zset.cpp
+++ 14/zset.cpp
@@ -1,6 +1,21 @@
 #include <assert.h>
+#include <cstddef>
+#include <cstdint>
 #include <stdlib.h>
 #include <string.h>
 // proj
+#include "avl.h"
 #include "common.h"
 #include "zset.h"
+
+static ZNode *znode_new(const char *name, size_t len, double score) {
+  ZNode *node = (ZNode *)malloc(sizeof(ZNode) + len);
+  assert(node); // not a good idea in real projects
+  avl_init(&node->tree);
+  node->hmap.next = NULL;
+  node->hmap.hcode = str_hash((uint8_t *)name, len);
+  node->score = score;
+  node->len = len;
+  memcpy(&node->name[0], name, len);
+  return node;
+}
