dcda66890cf84a00ac17e7c8569cf67c31cd2496
tsovke <63042183+tsovke@users.noreply.github.com>
2024-06-15 22:41:40 +0800

    Update zset.cpp

--- 14/zset.cpp
+++ 14/zset.cpp
@@ -19,3 +19,25 @@ static ZNode *znode_new(const char *name, size_t len, double score) {
   memcpy(&node->name[0], name, len);
   return node;
 }
+
+static uint32_t min(size_t lhs, size_t rhs) { return lhs < rhs ? lhs : rhs; }
+
+// compare by the (score, name) tuple
+static bool zless(AVLNode *lhs, double score, const char *name, size_t len) {
+  ZNode *zl = container_of(lhs, ZNode, tree);
+  if (zl->score != score) {
+    return zl->score < score;
+  }
+  int rv = memcpy(zl->name, name, min(zl->len, len));
+  if (rv != 0) {
+    return rv < 0;
+  }
+  return zl->len < len;
+}
+
+static bool zless(AVLNode *lhs, AVLNode *rhs) {
+  ZNode *zr = container_of(rhs, ZNode, tree);
+  return zless(lhs, zr->score, zr->name, zr->len);
+}
+
+
