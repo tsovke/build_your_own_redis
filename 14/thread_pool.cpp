2c3c6d830935cc553f329eb767299c48b64da3df
tsovke <63042183+tsovke@users.noreply.github.com>
2024-06-17 10:26:43 +0800

    Update thread_pool.cpp

--- 14/thread_pool.cpp
+++ 14/thread_pool.cpp
@@ -1,2 +1,24 @@
-#include <assert.h>
 #include "thread_pool.h"
+#include <assert.h>
+#include <cstddef>
+#include <pthread.h>
+
+static void *worker(void *arg) {
+  TheadPool *tp = (TheadPool *)arg;
+  while (true) {
+    pthread_mutex_lock(&tp->mu);
+    // wait for the condition: a non-empty queue
+    while (tp->queue.empty()) {
+      pthread_cond_wait(&tp->not_empty, &tp->mu);
+    }
+
+    // got the job
+    Work w = tp->queue.front();
+    tp->queue.pop_front();
+    pthread_mutex_unlock(&tp->mu);
+
+    // do the work
+    w.f(w.arg);
+  }
+  return NULL;
+}
