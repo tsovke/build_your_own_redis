42fef125c4e325b0f1e278d032e2d4d76edb36af
tsovke <63042183+tsovke@users.noreply.github.com>
2024-06-17 10:22:00 +0800

    ok

---
+++ 14/14_server.cpp
@@ -0,0 +1,175 @@
+#include <arpa/inet.h>
+#include <assert.h>
+#include <bits/types/error_t.h>
+#include <cstddef>
+#include <cstdint>
+#include <cstdlib>
+#include <ctime>
+#include <errno.h>
+#include <fcntl.h>
+#include <math.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <poll.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <string>
+#include <sys/socket.h>
+#include <time.h>
+#include <unistd.h>
+#include <vector>
+// proj
+#include "common.h"
+#include "hashtable.h"
+#include "heap.h"
+#include "list.h"
+#include "thread_pool.h"
+#include "zset.h"
+
+static void msg(const char *msg) { fprintf(stderr, "%s\n", msg); }
+
+static void die(const char *msg) {
+  int err = errno;
+  fprintf(stderr, "[%d] %s\n", err, msg);
+  abort();
+}
+
+static uint32_t get_monotonic_usec() {
+  timespec tv = {0, 0};
+  clock_gettime(CLOCK_MONOTONIC, &tv);
+  return uint64_t(tv.tv_sec) * 1000000 + tv.tv_nsec / 1000;
+}
+
+static void fd_set_nb(int fd) {
+  errno = 0;
+  int flags = fcntl(fd, F_GETFL, 0);
+  if (errno) {
+    die("fcntl error");
+    return;
+  }
+
+  flags |= O_NONBLOCK;
+
+  errno = 0;
+  (void)fcntl(fd, F_SETFL, flags);
+  if (errno) {
+    die("fcntl error");
+  }
+}
+
+struct Conn;
+
+// global variables
+static struct {
+  HMap db;
+  // a map of all client connections,keyed by fd
+  std::vector<Conn *> fd2conn;
+  // timers for idle connections
+  DList idle_list;
+  // timers for TTLs
+  std::vector<HeapItem> heap;
+  // the thread pool
+  TheadPool tp;
+} g_data;
+
+const size_t k_max_msg = 4096;
+
+enum {
+  STATE_REQ = 0,
+  STATE_RES = 1,
+  STATE_END = 2, // mark the connection for deletion
+};
+
+struct Conn {
+  int fd = -1;
+  uint32_t state = 0; // either STATE_REQ or STATE_RES
+  // buffer for reading
+  size_t rbuf_size = 0;
+  uint8_t rbuf[4 + k_max_msg];
+  // buffer for writing
+  size_t wbuf_size = 0;
+  size_t wbuf_sent = 0;
+  uint8_t wbuf[4 + k_max_msg];
+  uint64_t idle_start = 0;
+  // timer
+  DList idle_list;
+};
+
+static void conn_put(std::vector<Conn *> &fd2conn, struct Conn *conn) {
+  if (fd2conn.size() <= (size_t)conn->fd) {
+    fd2conn.resize(conn->fd + 1);
+  }
+  fd2conn[conn->fd] = conn;
+}
+
+static int32_t accept_new_conn(int fd) {
+  // accept
+  struct sockaddr_in client_addr = {};
+  socklen_t socklen = sizeof(client_addr);
+  int connfd = accept(fd, (struct sockaddr *)&client_addr, &socklen);
+  if (connfd < 0) {
+    msg("accept() error");
+    return -1; // error
+  }
+
+  // set the new connection fd to nonblocking mode
+  fd_set_nb(connfd);
+  // creating the struct Conn
+  struct Conn *conn = (struct Conn *)malloc(sizeof(struct Conn));
+  if (!conn) {
+    close(connfd);
+    return -1;
+  }
+  conn->fd = connfd;
+  conn->state = STATE_REQ;
+  conn->rbuf_size = 0;
+  conn->wbuf_size = 0;
+  conn->wbuf_sent = 0;
+  conn->idle_start = get_monotonic_usec();
+  dlist_insert_before(&g_data.idle_list, &conn->idle_list);
+  conn_put(g_data.fd2conn, conn);
+  return 0;
+}
+
+static void state_req(Conn *conn);
+static void state_res(Conn *conn);
+
+const size_t k_max_args = 1024;
+
+static int32_t parse_req(const uint8_t *data, size_t len,
+                         std::vector<std::string> &out) {
+  if (len < 4) {
+    return -1;
+  }
+  uint32_t n = 0;
+  memcpy(&n, &data[0], 4);
+  if (n > k_max_args) {
+    return -1;
+  }
+
+  size_t pos = 4;
+  while (n--) {
+    if (pos + 4 > len) {
+      return -1;
+    }
+    uint32_t sz = 0;
+    memcpy(&sz, &data[pos], 4);
+    if (pos + 4 + sz > len) {
+      return -1;
+    }
+    out.push_back(std::string((char *)&data[pos + 4], sz));
+    pos += 4 + sz;
+  }
+
+  if (pos != len) {
+    return -1; // trailing garbage
+  }
+  return 0;
+}
+
+enum {
+  T_STR = 0,
+  T_ZSET = 1,
+};
