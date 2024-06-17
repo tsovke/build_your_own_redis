29469c5dd6ad2a5e3847d45ad5a75938b068553c
tsovke <63042183+tsovke@users.noreply.github.com>
2024-06-17 10:03:17 +0800

    Update thread_pool.cpp

--- 14/thread_pool.cpp
+++ 14/thread_pool.cpp
@@ -79,3 +79,18 @@ enum {
   STATE_RES = 1,
   STATE_END = 2, // mark the connection for deletion
 };
+
+struct Conn {
+  int fd = -1;
+  uint32_t state = 0; // either STATE_REQ or STATE_RES
+  // buffer for reading
+  size_t rbuf_size = 0;
+  uint8_t rbuf[4 + k_max_msg];
+  // buffer for writing
+  size_t wbuf_size = 0;
+  size_t wbuf_sent = 0;
+  uint8_t wbuf[4 + k_max_msg];
+  uint64_t idle_start = 0;
+  // timer
+  DList idle_list;
+};
