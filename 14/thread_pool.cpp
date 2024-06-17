616b95948853b64940c95ebbb67f0e1077525118
tsovke <63042183+tsovke@users.noreply.github.com>
2024-06-17 10:30:31 +0800

    Update thread_pool.cpp

--- 14/thread_pool.cpp
+++ 14/thread_pool.cpp
@@ -22,3 +22,18 @@ static void *worker(void *arg) {
   }
   return NULL;
 }
+
+void thread_pool_init(TheadPool *tp, size_t num_threads) {
+  assert(num_threads > 0);
+
+  int rv = pthread_mutex_init(&tp->mu, NULL);
+  assert(rv == 0);
+  rv = pthread_cond_init(&tp->not_empty, NULL);
+  assert(rv == 0);
+
+  tp->threads.resize(num_threads);
+  for (size_t i = 0; i < num_threads; ++i) {
+    int rv = pthread_create(&tp->threads[i], NULL, *worker, tp);
+    assert(rv == 0);
+  }
+}
