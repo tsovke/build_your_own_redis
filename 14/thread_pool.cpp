24da5c0eefee0333a7f69079c85c4db0376f94cc
tsovke <63042183+tsovke@users.noreply.github.com>
2024-06-17 09:49:05 +0800

    Update thread_pool.cpp

--- 14/thread_pool.cpp
+++ 14/thread_pool.cpp
@@ -1,23 +1,57 @@
+#include <arpa/inet.h>
 #include <assert.h>
-#include <math.h>
-#include <stdint.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
+#include <bits/types/error_t.h>
+#include <cstdint>
+#include <ctime>
 #include <errno.h>
 #include <fcntl.h>
-#include <poll.h>
-#include <unistd.h>
-#include <time.h>
-#include <arpa/inet.h>
-#include <sys/socket.h>
+#include <math.h>
 #include <netinet/ip.h>
+#include <poll.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <string>
+#include <sys/socket.h>
+#include <time.h>
+#include <unistd.h>
 #include <vector>
 // proj
-#include "hashtable.h"
-#include "zset.h"
-#include "list.h"
-#include "heap.h"
-#include "thread_pool.h"
 #include "common.h"
+#include "hashtable.h"
+#include "heap.h"
+#include "list.h"
+#include "thread_pool.h"
+#include "zset.h"
+
+static void msg(const char *msg) { fprintf(stderr, "%s\n", msg); }
+
+static void die(const char *msg) {
+  int err = errno;
+  fprintf(stderr, "[%d] %s\n", err, msg);
+  abort();
+}
+
+static uint32_t get_monotonic_usec() {
+  timespec tv = {0, 0};
+  clock_gettime(CLOCK_MONOTONIC, &tv);
+  return uint64_t(tv.tv_sec) * 1000000 + tv.tv_nsec / 1000;
+}
+
+static void fd_set_nb(int fd) {
+  errno = 0;
+  int flags = fcntl(fd, F_GETFL, 0);
+  if (errno) {
+    die("fcntl error");
+    return;
+  }
+
+  flags |= O_NONBLOCK;
+
+  errno = 0;
+  (void)fcntl(fd, F_SETFL, flags);
+  if (errno) {
+    die("fcntl error");
+  }
+}
