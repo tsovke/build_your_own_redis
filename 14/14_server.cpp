e00e370e6bdf1e5edaddfc245f8fd463ed59642d
tsovke <63042183+tsovke@users.noreply.github.com>
2024-06-20 00:34:54 +0800

    Update 14_server.cpp

--- 14/14_server.cpp
+++ 14/14_server.cpp
@@ -262,3 +262,26 @@ static void do_get(std::vector<std::string> &cmd, std::string &out) {
   }
   return out_str(out, ent->val);
 }
+
+static void do_set(std::vector<std::string> &cmd, std::string &out) {
+
+  Entry key;
+  key.key.swap(cmd[1]);
+  key.node.hcode = str_hash((uint8_t *)key.key.data(), key.key.size());
+
+  HNode *node = hm_lookup(&g_data.db, &key.node, &entry_eq);
+  if (node) {
+    Entry *ent = container_of(node, Entry, node);
+    if (ent->type != T_STR) {
+      return out_err(out, ERR_TYPE, "expect string type");
+    }
+    ent->val.swap(cmd[2]);
+  } else {
+    Entry *ent = new Entry();
+    ent->key.swap(key.key);
+    ent->node.hcode = key.node.hcode;
+    ent->val.swap(cmd[2]);
+    hm_insert(&g_data.db, &ent->node);
+  }
+  return out_nil(out);
+}
