3d25ccd18411ff7e9604beaf4e5558f765ce38c2
tsovke <tsovke@gmail.com>
2024-06-23 01:14:46 +0800

    ok

--- 14/14_server.cpp
+++ 14/14_server.cpp
@@ -1,6 +1,7 @@
 #include <arpa/inet.h>
 #include <assert.h>
 #include <bits/types/error_t.h>
+#include <cerrno>
 #include <cmath>
 #include <cstddef>
 #include <cstdint>
@@ -17,7 +18,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <string>
+#include <strings.h>
 #include <sys/socket.h>
+#include <sys/types.h>
 #include <time.h>
 #include <unistd.h>
 #include <vector>
@@ -482,4 +485,207 @@ static bool expect_zset(std::string &out, std::string &s, Entry **ent) {
     return false;
   }
   return true;
+}
+
+// zrem zset name
+static void do_zrem(std::vector<std::string> &cmd, std::string &out) {
+  Entry *ent = NULL;
+  if (!expect_zset(out, cmd[1], &ent)) {
+    return;
+  }
+
+  const std::string &name = cmd[2];
+  ZNode *znode = zset_pop(ent->zset, name.data(), name.size());
+  if (znode) {
+    znode_del(znode);
+  }
+  return out_int(out, znode ? 1 : 0);
+}
+
+// zscore zset name
+static void do_zscore(std::vector<std::string> &cmd, std::string &out) {
+  Entry *ent = NULL;
+  if (!expect_zset(out, cmd[1], &ent)) {
+    return;
+  }
+
+  const std::string &name = cmd[2];
+  ZNode *znode = zset_lookup(ent->zset, name.data(), name.size());
+  return znode ? out_dbl(out, znode->score) : out_nil(out);
+}
+
+// zquery zset score name offset limit
+static void do_zquery(std::vector<std::string> &cmd, std::string &out) {
+  // parse args
+  double score = 0;
+  if (!str2dbl(cmd[2], score)) {
+    return out_err(out, ERR_ARG, "expect fp number");
+  }
+  const std::string &name = cmd[3];
+  int64_t offset = 0;
+  int64_t limit = 0;
+  if (!str2int(cmd[4], offset)) {
+    return out_err(out, ERR_ARG, "expect int");
+  }
+  if (!str2int(cmd[5], limit)) {
+    return out_err(out, ERR_ARG, "expect int");
+  }
+
+  // get the zset
+  Entry *ent = NULL;
+  if (!expect_zset(out, cmd[1], &ent)) {
+    if (out[0] == SER_NIL) {
+      out.clear();
+      out_arr(out, 0);
+    }
+    return;
+  }
+
+  // look up the tuple
+  if (limit <= 0) {
+    return out_arr(out, 0);
+  }
+  ZNode *znode = zset_query(ent->zset, score, name.data(), name.size());
+  znode = znode_offset(znode, offset);
+
+  // output
+  void *arr = begin_arr(out);
+  uint32_t n = 0;
+  while (znode && (int64_t)n < limit) {
+    out_str(out, znode->name, znode->len);
+    out_dbl(out, znode->score);
+    znode = znode_offset(znode, +1);
+    n += 2;
+  }
+  end_arr(out, arr, n);
+}
+
+static bool cmd_is(const std::string &word, const char *cmd) {
+  return 0 == strcasecmp(word.c_str(), cmd);
+}
+
+static void do_request(std::vector<std::string> &cmd, std::string &out) {
+  if (cmd.size() == 1 && cmd_is(cmd[0], "keys")) {
+    do_keys(cmd, out);
+  } else if (cmd.size() == 2 && cmd_is(cmd[0], "get")) {
+    do_get(cmd, out);
+  } else if (cmd.size() == 3 && cmd_is(cmd[0], "set")) {
+    do_set(cmd, out);
+  } else if (cmd.size() == 2 && cmd_is(cmd[0], "del")) {
+    do_del(cmd, out);
+  } else if (cmd.size() == 3 && cmd_is(cmd[0], "pexpire")) {
+    do_expire(cmd, out);
+  } else if (cmd.size() == 2 && cmd_is(cmd[0], "pttl")) {
+    do_ttl(cmd, out);
+  } else if (cmd.size() == 4 && cmd_is(cmd[0], "zadd")) {
+    do_zadd(cmd, out);
+  } else if (cmd.size() == 3 && cmd_is(cmd[0], "zrem")) {
+    do_zrem(cmd, out);
+  } else if (cmd.size() == 3 && cmd_is(cmd[0], "zscore")) {
+    do_zscore(cmd, out);
+  } else if (cmd.size() == 6 && cmd_is(cmd[0], "zquery")) {
+    do_zquery(cmd, out);
+  } else {
+    // cmd is not recognized
+    out_err(out, ERR_UNKNOWN, "Unknown cmd");
+  }
+}
+
+static bool try_one_request(Conn *conn) {
+  // try to parse a requst from the buffer
+  if (conn->rbuf_size < 4) {
+    // not enough data in the buffer. Will retry in the next iteration
+    return false;
+  }
+  uint32_t len = 0;
+  memcpy(&len, &conn->rbuf[0], 4);
+  if (len > k_max_msg) {
+    msg("too long");
+    conn->state = STATE_END;
+    return false;
+  }
+  if (4 + len > conn->rbuf_size) {
+    // not enough data in the buffer. Will retry in the next iteration
+    return false;
+  }
+
+  // parse the request
+  std::vector<std::string> cmd;
+  if (0 != parse_req(&conn->rbuf[4], len, cmd)) {
+    msg("bad req");
+    conn->state = STATE_END;
+    return false;
+  }
+
+  // got one request, generate the response.
+  std::string out;
+  do_request(cmd, out);
+
+  // pack the response into the buffer
+  if (4 + out.size() > k_max_msg) {
+    out.clear();
+    out_err(out, ERR_2BIG, "response is too big");
+  }
+  uint32_t wlen = (uint32_t)out.size();
+  memcpy(&conn->wbuf[0], &wlen, 4);
+  memcpy(&conn->wbuf[4], out.data(), out.size());
+  conn->wbuf_size = 4 + wlen;
+
+  // remove the request from the buffer.
+  // note: frequent memmove is inefficient.
+  // note: need better handling for production code.
+  size_t remain = conn->rbuf_size - 4 - len;
+  if (remain) {
+    memmove(conn->rbuf, &conn->rbuf[4 + len], remain);
+  }
+  conn->rbuf_size = remain;
+
+  // change state
+  conn->state = STATE_RES;
+  state_res(conn);
+
+  // continue the outer loop if the request was fully processed
+  return (conn->state == STATE_REQ);
+}
+
+static bool try_fill_buffer(Conn *conn) {
+  // try to fill the buffer
+  assert(conn->rbuf_size < sizeof(conn->rbuf));
+  ssize_t rv = 0;
+  do {
+    size_t cap = sizeof(conn->rbuf) - conn->rbuf_size;
+    rv = read(conn->fd, &conn->rbuf[conn->rbuf_size], cap);
+  } while (rv < 0 && errno == EINTR);
+  if (rv < 0 && errno == EAGAIN) {
+    // got EAGAIN, stop.
+    return false;
+  }
+  if (rv < 0) {
+    msg("read() error");
+    conn->state = STATE_END;
+    return false;
+  }
+  if (rv == 0) {
+    if (conn->rbuf_size > 0) {
+      msg("unexpected EOF");
+    } else {
+      msg("EOF");
+    }
+    conn->state = STATE_END;
+    return false;
+  }
+
+  conn->rbuf_size += (size_t)rv;
+  assert(conn->rbuf_size <= sizeof(conn->rbuf));
+
+  // Try to process requests one by one.
+  // Why is there a loop? Please read the explanation of "pipelining".
+  while (try_one_request(conn)) {
+  }
+  return (conn->state == STATE_REQ);
+}
+
+static void state_req(Conn *conn) {
+  while (try_fill_buffer(conn)) {
+  }
 }
